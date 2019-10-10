51148f7c4022b7fc1a2d618a13faffbecb3df29f
pranavjoy <pranavjoy@protonmail.com>
2019-10-10 17:26:49 +0200

    Changed accepted file extensions

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ UPLOAD_FOLDER = './uploads'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['gif', 'mp4', 'MP4', 'mov', 'MOV', 'mkv', 'MKV', 'GIF'])
 
 
 def allowed_file(filename):
